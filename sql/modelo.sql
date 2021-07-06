d6ed4c31c15b715c1b028754e5d3aef3548c63e8
Erick Barrera Lopez <erick.barrera@mercadolibre.com.mx>
2021-07-06 13:00:28 -0500

    Adds model

---
+++ sql/modelo.sql
@@ -0,0 +1,13 @@
+create database if not exists red-social;
+use red-social;
+
+drop table if exists usuarios;
+
+create table usuarios(
+    id int auto_increment primary key,
+    nome varchar(50) not null,
+    nick varchar(50) not null unique,
+    email varchar(50) not null unique,
+    senha varchar(50) not null unique,
+    criadoEm timestamp default current_timestamp()
+) ENGINE=INNODB;
