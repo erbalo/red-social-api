8de068fdf0f38cfbb72c764966999c9a82be9f57
Erick Barrera Lopez <erick.barrera@mercadolibre.com.mx>
2021-07-07 11:56:37 -0500

    Adds user validations

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -21,17 +21,25 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	var usuario modelos.Usuario
 	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
 		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = usuario.Preparar(); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
 	}
 
 	db, erro := banco.Conectar()
 	if erro != nil {
 		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
 
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
 	usuario.ID, erro = repositorio.Criar(usuario)
 	if erro != nil {
 		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
 
 	respostas.JSON(w, http.StatusCreated, usuario)
--- src/modelos/usuario.go
+++ src/modelos/usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario representa um usuario utilizando a rede social
 type Usuario struct {
@@ -10,4 +14,41 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+// Preparar vai chamar os metodos para valdar e formatar o usuario recebido
+func (usuario *Usuario) Preparar() error {
+	usuario.formatar()
+
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome e obrigatorio e nao pode estar em branco")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O nick e obrigatorio e nao pode estar em branco")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O email e obrigatorio e nao pode estar em branco")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("A senha e obrigatoria e nao pode estar em branco")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
