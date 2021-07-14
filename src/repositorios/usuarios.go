a0d74be3d8d303c1a9aa8a15650d9a3eba1831a4
Erick Barrera Lopez <erick.barrera@mercadolibre.com.mx>
2021-07-14 15:43:57 -0500

    Adds follow user endpoint

--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -137,3 +137,18 @@ func (repositorio Usuarios) BuscarPorEmail(email string) (modelos.Usuario, error
 
 	return usuario, nil
 }
+
+// Seguir permite que um usuario siga outro
+func (repositorio Usuarios) Seguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare("insert ignore into seguidores (usuarioid, seguidor_id) values (?,?)")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
