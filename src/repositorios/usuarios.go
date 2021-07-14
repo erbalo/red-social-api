6704be8c483557208bf55e872def1e46c5ac8fb1
Erick Barrera Lopez <erick.barrera@mercadolibre.com.mx>
2021-07-14 15:55:10 -0500

    Adds delete followers endpoint

--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -152,3 +152,17 @@ func (repositorio Usuarios) Seguir(usuarioID, seguidorID uint64) error {
 
 	return nil
 }
+
+// PararDeSeguir permite que um usuario pare de seguir o outro
+func (repositorio Usuarios) PararDeSeguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from seguidores where usuario_id = ? and seguidor_id = ?")
+	if erro != nil {
+		return erro
+	}
+
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
