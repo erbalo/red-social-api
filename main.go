95905350094ad9eff086c2ec410238ee009d1d01
Erick Barrera Lopez <erick.barrera@mercadolibre.com.mx>
2021-07-14 12:36:33 -0500

    Adds middlewares

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 	"net/http"
 )
 
-/*func init() {
+/* Solo usamos esta funcion para crear de forma segura una llave para el archivo .env
+func init() {
 	chave := make([]byte, 64)
 
 	if _, erro := rand.Read(chave); erro != nil {
@@ -17,7 +18,8 @@ import (
 
 	stringBase64 := base64.StdEncoding.EncodeToString(chave)
 	fmt.Println(stringBase64)
-}*/
+}
+*/
 
 func main() {
 	config.Carregar()
